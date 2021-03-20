f2c16abdc433e09fe7594f693f518810baf92ff4
maryvillondo <maryvillondo@gmail.com>
2021-03-20 18:12:48 +0800

    Revise

--- metroevents/metroevents/settings.py
+++ metroevents/metroevents/settings.py
@@ -134,7 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,  'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
+
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
