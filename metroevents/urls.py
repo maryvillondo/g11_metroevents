6650eedfa80e9688085192166c0814c7be40c16e
maryvillondo <maryvillondo@gmail.com>
2021-03-24 20:03:23 +0800

    Update urls.py

--- metroevents/urls.py
+++ metroevents/urls.py
@@ -25,7 +25,6 @@ urlpatterns = [
     path('', include('guest.urls', namespace='guest')),
     path('organizer/', include('organizer.urls', namespace='organizer')),
     path('user/', include('user.urls', namespace='user'))
-] 
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
